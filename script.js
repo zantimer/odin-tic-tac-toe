67409ab8e115b9348194cdc07a4c8e45bcad66e1
zantimer <karol.nikiel1@gmail.com>
2022-11-20 22:31:47 +0000

    wrote sum spaghetti, oh my God this is terrifying, will write more as we go

--- script.js
+++ script.js
@@ -1,55 +1,137 @@
+//global queries
+const xButton = document.querySelector('.X');
+const oButton = document.querySelector('.O');
+const winnerField = document.querySelector('.winnerField');
 //game logic
 
 const gameBoard = (()=>
 {
-    const fields = {topLeft:'X',
-    topCenter: 'X',
-    topRight: 'X',
-    centerLeft:'X',
-    center: 'X',
-    centerRight: 'X',
-    bottomLeft:'X',
-    bottomCenter: 'X',
-    bottomRight: 'X',}
+    const fields = {topLeft:'',
+    topCenter: '',
+    topRight: '',
+    centerLeft:'',
+    center: '',
+    centerRight: '',
+    bottomLeft:'',
+    bottomCenter: '',
+    bottomRight: ''}
+
+    switch (fields) {
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+            winnerField.textContent = "something, somehow";
+            break;
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+            winnerField.textContent = "something, somehow";
+            break;
+            
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+            winnerField.textContent = "something, somehow";
+            break;
+        
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+        winnerField.textContent = "something, somehow";
+        break;
+        
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+        winnerField.textContent = "something, somehow";
+        break;
+        
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+        winnerField.textContent = "something, somehow";
+        break;
+        
+        case fields[0]==fields[1] && fields[1]==fields[2]:
+            winnerField.textContent = "something, somehow";
+            break;
+        
+            case fields[0]==fields[1] && fields[1]==fields[2]:
+            winnerField.textContent = "something, somehow";
+            break;
+    
+        default:
+            break;
+    }
 
     return fields;
 })();
 
-const gameFlow = ((playerA, playerB)=>
-{
-    //1. decide whose turn it is
-    //2. decide whose turn is it next
-    //3. decide what marker to put down
-    //4. decide where
-    //5. pass over turn to next player
-    
-    const playerTurns = {current:undefined,
-    next: undefined}
-    if(playerA.marker == 'X')
-    {
-        playerTurns.current = playerA;
-        playerTurns.next = playerB;
-    }
-    else
-    {
-        playerTurns.current = playerB;
-        playerTurns.next = playerA;
-    }
-
-})();
-
 //player factory logic
+const playerFactory = (name, who, marker) => 
+{
+    const isTurnOf = false;        
+    return {name, who, marker, isTurnOf}
+};
 
-const playerFactory = (name) =>{
-    const sayName = () => console.log(`my name is ${name}`);
-    return {sayName};
+const humanPlayer = playerFactory(prompt('Input name'),
+ 'human');
+const aiPlayer = playerFactory('John but AI', 
+'ai')
+
+//game logic
+xButton.addEventListener('click', () =>{
+    humanPlayer.marker = 'X';
+    aiPlayer.marker = 'O';
+    drawBoard();
+    xButton.disabled = true;
+    oButton.disabled = true;
+})
+oButton.addEventListener('click', () =>{
+    humanPlayer.marker = 'O';
+    aiPlayer.marker = 'X';
+    xButton.disabled = true;
+    oButton.disabled = true;
+})
+
+const gameFlow = (()=>{
+    const turns = {first: undefined,
+                second: undefined};
+        if(humanPlayer.marker == 'O')
+        {
+            turns.first = humanPlayer;
+            turns.second = aiPlayer;
+        }
+        else
+        {
+            turns.first = aiPlayer;
+            turns.second = humanPlayer;
+        }
+        turns.first.isTurnOf = true;
+        while (turns.first.isTurnOf = true)
+        {
+            //mark the board
+            //wait for current player field choice
+            //pass the turn to other player
+        }
+})
+
+function drawBoard()
+{
+    for (field in gameBoard)
+    {
+        const div = document.createElement('div');
+        const p = document.createElement('p');
+
+        div.classList.add('field');
+        div.classList.add(field);
+        p.classList.add('field-value');
+        p.textContent = gameBoard[field];
+
+        document.body.appendChild(div);
+        div.appendChild(p);
+        
+
+        humanMarkerDown(p, div);
+    }
+
+    function humanMarkerDown(p, div) {
+        p.addEventListener('click', (e) => {
+            if (humanPlayer.isTurnOf && e.target.textContent == '') {
+                e.target.textContent = humanPlayer.marker;
+                gameBoard[div.classList.item(1)] = humanPlayer.marker;
+            }
+        });
+    }
 }
 
-const actualPlayer = (name) => {
-    const {sayName} = playerFactory(name);
-    const pickLogic = () => console.log('Pick logic will be here');
-    return {sayName, pickLogic};
-}
 
-const humanPlayer = actualPlayer('John doe');
-
+humanPlayer.isTurnOf = true;
