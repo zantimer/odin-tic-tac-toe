e572d35538874875d30c5b91eca7c7df5b49d014
zantimer <karol.nikiel1@gmail.com>
2022-11-22 12:33:49 +0000

    finished random AI logic, maybe will do minimax

--- script.js
+++ script.js
@@ -34,12 +34,13 @@ const aiPlayer = playerFactory('John but AI',
 
 //game logic
 
+let winner = false;
 const gameFlow = (()=>{
     const turns = {first: undefined,
                 second: undefined};
         function startGame(){
             
-            
+        
         if(humanPlayer.marker == 'O' && !humanPlayer.hasMoved)
         {
             turns.first = humanPlayer;
@@ -52,8 +53,10 @@ const gameFlow = (()=>{
             turns.second = humanPlayer;
         }
     
+        if(!winner)
+        {
         turns.first.isTurnOf = true;
-        
+        }
         if(turns.first == aiPlayer && 
             (gameBoard.fields.f1 != 'X'||'O' &&
             gameBoard.fields.f2 != 'X'||'O' &&
@@ -63,7 +66,8 @@ const gameFlow = (()=>{
             gameBoard.fields.f6 != 'X'||'O' &&
             gameBoard.fields.f7 != 'X'||'O' &&
             gameBoard.fields.f8 != 'X'||'O' &&
-            gameBoard.fields.f9 != 'X'||'O'))
+            gameBoard.fields.f9 != 'X'||'O') && aiPlayer.isTurnOf
+            && !winner)
         {
             console.log('my turn now')
             function randomField(min, max)
@@ -99,7 +103,7 @@ const gameFlow = (()=>{
         }
         
 
-
+        
 
         xButton.addEventListener('click', () =>{
             humanPlayer.marker = 'X';
@@ -114,87 +118,7 @@ const gameFlow = (()=>{
             drawBoard();
             xButton.disabled = true;
             oButton.disabled = true;
-        })
-        function CheckWinner(gameBoard, p) {
-            p.addEventListener('click', () =>{
-                
-            if ((gameBoard.fields['f1'] == 'X' &&
-                gameBoard.fields['f2'] == 'X' &&
-                gameBoard.fields['f3'] == 'X') ||
-                (gameBoard.fields['f1'] == 'O' &&
-                gameBoard.fields['f2'] == 'O' &&
-                gameBoard.fields['f3'] == 'O'))
-                {
-                    winnerField.textContent = "something, somehow";
-                
-                }
-                
-                else if ((gameBoard.fields['f4'] == 'X' &&
-                gameBoard.fields['f5'] == 'X' &&
-                gameBoard.fields['f6'] == 'X' )||
-                (gameBoard.fields['f4'] == 'O' &&
-                gameBoard.fields['f5'] == 'O' &&
-                gameBoard.fields['f6'] == 'O'))
-                {
-                winnerField.textContent = "something, somehow";
-                
-                }
-                
-                else if ((gameBoard.fields['f7'] == 'X' &&
-                gameBoard.fields['f8'] == 'X' &&
-                gameBoard.fields['f9'] == 'X') ||
-                (gameBoard.fields['f7'] == 'O' &&
-                gameBoard.fields['f8'] == 'O' &&
-                gameBoard.fields['f9'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-
-            else if ((gameBoard.fields['f1'] == 'X' &&
-                gameBoard.fields['f4'] == 'X' &&
-                gameBoard.fields['f7'] == 'X' )||
-                (gameBoard.fields['f1'] == 'O' &&
-                gameBoard.fields['f4'] == 'O' &&
-                gameBoard.fields['f7'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-                
-                else if ((gameBoard.fields['f2'] == 'X' &&
-                gameBoard.fields['f5'] == 'X' &&
-                gameBoard.fields['f8'] == 'X') ||
-                (gameBoard.fields['f2'] == 'O' &&
-                gameBoard.fields['f5'] == 'O' &&
-                gameBoard.fields['f8'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-                
-                else if ((gameBoard.fields['f3'] == 'X' &&
-                gameBoard.fields['f4'] == 'X' &&
-                gameBoard.fields['f9'] == 'X') ||
-                (gameBoard.fields['f3'] == 'O' &&
-                gameBoard.fields['f4'] == 'O' &&
-                gameBoard.fields['f9'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-                
-                else if ((gameBoard.fields['f1'] == 'X' &&
-                gameBoard.fields['f5'] == 'X' &&
-                gameBoard.fields['f9'] == 'X') ||
-                (gameBoard.fields['f1'] == 'O' &&
-                gameBoard.fields['f5'] == 'O' &&
-                gameBoard.fields['f9'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-                
-            else if ((gameBoard.fields['f3'] == 'X' &&
-                gameBoard.fields['f5'] == 'X' &&
-                gameBoard.fields['f7'] == 'X') ||
-                (gameBoard.fields['f3'] == 'O' &&
-                gameBoard.fields['f5'] == 'O' &&
-                gameBoard.fields['f7'] == 'O'))
-                {winnerField.textContent = "something, somehow";
-                }
-            })
-                }
+        })      
         
         function drawBoard()
         {
@@ -213,17 +137,22 @@ const gameFlow = (()=>{
             
             
             humanMarkerDown(p, div);
-            CheckWinner(gameBoard, p);
         }
+        const divs = document.querySelectorAll('#field');
+        divs.forEach(div =>{
+            div.addEventListener('click',()=>{winnerCheck.CheckWinner(gameBoard)});
+        })
         
         function humanMarkerDown(p, div) {
             p.addEventListener('click', (e) => {
-                if (humanPlayer.isTurnOf && e.target.textContent == '') {
+                if (humanPlayer.isTurnOf && e.target.textContent == ''&&!winner) {
                     e.target.textContent = humanPlayer.marker;
                     gameBoard.fields[div.classList.item(0)] = humanPlayer.marker;
-                    humanPlayer.isTurnOf = false;
+                    
+                    if (!winnerCheck.CheckWinner(gameBoard))
+                    {humanPlayer.isTurnOf = false;
                     turns.first = aiPlayer;
-                    startGame();
+                    startGame();}
                 }
             });
         }
@@ -249,6 +178,7 @@ const gameFlow = (()=>{
             winnerField.textContent = '';
             xButton.disabled = false;
             oButton.disabled = false;
+            winner = false;
             
         }
         const resetBtn = document.createElement('button');
@@ -256,5 +186,153 @@ const gameFlow = (()=>{
         resetBtn.classList.add('resetBtn');
         resetBtn.textContent = "Reset game"
         resetBtn.addEventListener('click', resetGame);
+
+        const winnerCheck = {CheckWinner:
+        function CheckWinner(gameBoard) {
+            if (!winner)    
+            {
+                if ((gameBoard.fields['f1'] == humanPlayer.marker &&
+                gameBoard.fields['f2'] == humanPlayer.marker &&
+                gameBoard.fields['f3'] == humanPlayer.marker) ||
+                (gameBoard.fields['f1'] == aiPlayer.marker &&
+                gameBoard.fields['f2'] == aiPlayer.marker &&
+                gameBoard.fields['f3'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f1'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                     humanPlayer.isTurnOf = false;
+                     winner = true;
+                     console.log('conditional 1');
+                     return true;
+                }
+                
+                else if ((gameBoard.fields['f4'] == humanPlayer.marker &&
+                gameBoard.fields['f5'] == humanPlayer.marker &&
+                gameBoard.fields['f6'] == humanPlayer.marker )||
+                (gameBoard.fields['f4'] == aiPlayer.marker &&
+                gameBoard.fields['f5'] == aiPlayer.marker &&
+                gameBoard.fields['f6'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f4'] ==
+                    humanPlayer.marker ?
+                    `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                    aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 2');
+                    return true;
+                }
+                
+                else if ((gameBoard.fields['f7'] == humanPlayer.marker&&
+                gameBoard.fields['f8'] == humanPlayer.marker &&
+                gameBoard.fields['f9'] == humanPlayer.marker) ||
+                (gameBoard.fields['f7'] == aiPlayer.marker &&
+                gameBoard.fields['f8'] == aiPlayer.marker &&
+                gameBoard.fields['f9'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f7'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 3');
+                    return true;
+                }
+
+            else if ((gameBoard.fields['f1'] == humanPlayer.marker &&
+                gameBoard.fields['f4'] == humanPlayer.marker &&
+                gameBoard.fields['f7'] == humanPlayer.marker )||
+                (gameBoard.fields['f1'] == aiPlayer.marker &&
+                gameBoard.fields['f4'] == aiPlayer.marker &&
+                gameBoard.fields['f7'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f1'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 4');
+                    return true;
+                }
+                
+                else if ((gameBoard.fields['f2'] == humanPlayer.marker &&
+                gameBoard.fields['f5'] == humanPlayer.marker &&
+                gameBoard.fields['f8'] == humanPlayer.marker) ||
+                (gameBoard.fields['f2'] == aiPlayer.marker &&
+                gameBoard.fields['f5'] == aiPlayer.marker &&
+                gameBoard.fields['f8'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f2'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 5');
+                    return true;
+                }
+                
+                else if ((gameBoard.fields['f3'] == humanPlayer.marker &&
+                gameBoard.fields['f6'] == humanPlayer.marker &&
+                gameBoard.fields['f9'] == humanPlayer.marker) ||
+                (gameBoard.fields['f3'] == aiPlayer.marker &&
+                gameBoard.fields['f6'] == aiPlayer.marker &&
+                gameBoard.fields['f9'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f3'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 6');
+                    return true;
+                }
+                
+                else if ((gameBoard.fields['f1'] == humanPlayer.marker &&
+                gameBoard.fields['f5'] == humanPlayer.marker &&
+                gameBoard.fields['f9'] == humanPlayer.marker) ||
+                (gameBoard.fields['f1'] == aiPlayer.marker &&
+                gameBoard.fields['f5'] == aiPlayer.marker &&
+                gameBoard.fields['f9'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f1'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 7');
+                    return true;
+                }
+                
+            else if ((gameBoard.fields['f3'] == humanPlayer.marker &&
+                gameBoard.fields['f5'] == humanPlayer.marker &&
+                gameBoard.fields['f7'] == humanPlayer.marker) ||
+                (gameBoard.fields['f3'] == aiPlayer.marker &&
+                gameBoard.fields['f5'] == aiPlayer.marker &&
+                gameBoard.fields['f7'] == aiPlayer.marker))
+                {
+                    winnerField.textContent = gameBoard.fields['f3'] ==
+                     humanPlayer.marker ?
+                     `${humanPlayer.name} won!`:`${aiPlayer.name} won!`;
+                     aiPlayer.isTurnOf = false;
+                    humanPlayer.isTurnOf = false;
+                    winner = true;
+                    console.log('conditional 8');
+                    return true;
+                }
+                else
+                {
+                return false;
+                }
+            }
+            }
+            
+        }
     })();
     
